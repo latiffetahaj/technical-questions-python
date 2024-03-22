8b42141c231f17177eb8dce5ecb56a5b8e21f03e
latiffetahaj <latiffetahaj@gmail.com>
2024-03-21 20:57:52 -0400

    added comments

--- __032__same_binary_tree.py
+++ __032__same_binary_tree.py
@@ -9,20 +9,23 @@
 
     #LeetCode: https://leetcode.com/problems/same-tree/description/
 
-    Time Complexity: O( p + q ) -   p - nr of nodes in first tree,
-                                    q - nr of nodes in the second tree.
+    Time Complexity: O(p + q) - p - nr of nodes in first tree,
+                                q - nr of nodes in the second tree.
 
-    Space Complexity: O(2^h) - where h is the height of the largest tree
+    Space Complexity: O(2^h) - where h is the height of the largest tree between the two
 '''
 
 def same_tree(p, q):
 
     if p and q:
+        #only if the values don't match, return False
         if p.val != q.val:
             return False
     elif not p and not q:
+        #if both not None
         return True
     else:
+        #if one of them is None
         return False
 
     return same_tree(p.left,q.left) and same_tree(p.right,q.right)
