31b5900328736f6bf1e34d6111ef9c8e0744fdff
latiffetahaj <latiffetahaj@gmail.com>
2024-04-04 22:36:24 -0400

    added clarification comments

--- G_SWEP/WEEK 2/print_permutations.py
+++ G_SWEP/WEEK 2/print_permutations.py
@@ -1,3 +1,13 @@
+'''
+    Print all permutations of a string.
+    Given a string ("123"), print all permutations of that string ("123", "231", "321", …)
+
+    Time Complexity: O(n!)
+    Space Complexity: O(n!) because there are n! recursive calls ?? (not so sure here)
+'''
+#idea is to use a buffer (extra array of len(input)) to store current permutation
+#also to use a boolean array that tells which items are currently in the buffer
+#everytime the buffer is full, append to the result
 def permute(nums):
     input_length = len(nums)
     isInBuffer = [False] * input_length
@@ -6,15 +16,22 @@ def permute(nums):
 
     def generate_perm(bufferIndex):
         nonlocal input_length, permutations,nums,buffer,isInBuffer
+
+        #when buffer is full
         if bufferIndex == input_length:
+            #append a copy of the buffer to avoid passed by reference
             permutations.append(buffer.copy())
             return
 
+        #find candiates to put in the buffer
         for i in range(input_length):
+            #consider only the elements that currently are not in the buffer
             if not isInBuffer[i]:
+                #place the element in the current buffer index
                 buffer[bufferIndex] = nums[i]
 
                 isInBuffer[i] = True
+                #recursive call on the next index
                 generate_perm(bufferIndex + 1)
                 isInBuffer[i] = False
 
